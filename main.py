8c1d0c3c13b4f3955e47237999ce22c3ea6df4d8
Quan Nguyen <quangineer@gmail.com>
2019-05-27 08:21:16 -0700

    clean data

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ df_18 = pd.read_csv ('all_alpha_18.csv')
 #df_18.duplicated().sum()
 #df_18.isnull().sum()
 #df_18["Fuel"].unique()
+
+df_08.drop(['Stnd', 'Underhood ID', 'FE Calc Appr', 'Unadj Cmb MPG'], axis=1, inplace=True)
+df_18.drop(['Stnd', 'Underhood ID', 'Stnd Description', 'Comb CO2'], axis=1, inplace=True)
+df_08 = df_08.rename(columns={"Sales Area":"Cert Region"})
+df_08.rename(columns=lambda x: x.strip().lower().replace(" ", "_"), inplace=True)
+df_18.rename(columns=lambda x: x.strip().lower().replace(" ", "_"), inplace=True)
+(df_08.columns == df_18.columns).all() # confirm column labels for 2008 and 2018 datasets are identical
+# save new datasets for next section
+df_08.to_csv('data_08_v1.csv', index=False)
+df_18.to_csv('data_18_v1.csv', index=False)
