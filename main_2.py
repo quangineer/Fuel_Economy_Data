f6e9dbf6913b4ddbfe80112025e4874c328d7f8c
Quan Nguyen <quangineer@gmail.com>
2019-05-28 15:17:57 -0700

    bug fixed

--- main_2.py
+++ main_2.py
@@ -3,8 +3,6 @@ import pandas as pd
 df_08 = pd.read_csv('data_08_v1.csv')
 df_18 = pd.read_csv('data_18_v1.csv')
 
-df_08.shape
-df_18.shape
 
 # Filter by Certification region:
 df_08 = df_08.query('cert_region == "CA"')
@@ -23,11 +21,14 @@ df_18.dropna(inplace=True)
 df_08.isnull().sum().any()
 df_18.isnull().sum().any()
 
+
 # Dedupe Data:
 df_08.duplicated().sum()
 df_18.duplicated().sum()
-df_08.drop_duplicates()
-df_18.drop_duplicates()
+df_08.drop_duplicates(inplace=True)
+df_18.drop_duplicates(inplace=True)
+print (df_08.shape)
+print (df_18.shape)
 df_08.duplicated().sum()
 df_18.duplicated().sum()
 df_08.to_csv('data_08_v2.csv', index=False)
