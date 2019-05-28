8af5aecb4e264c99af66c6687f80a9f73f41d38f
Quan Nguyen <quangineer@gmail.com>
2019-05-28 10:57:46 -0700

    updates

--- main_4.py
+++ main_4.py
@@ -12,3 +12,30 @@ df_18 = pd.read_csv('data_18_v3.csv')
 hb_08 = df_08[df_08['fuel'].str.contains('/')]  # pull out all the cell that has the value containing '/'
 hb_18 = df_18[df_18['fuel'].str.contains('/')]
 
+# After the step above is executed, we now have two sets of datas namely hb_08 and hb_18.
+# We have to crack each 'hybrid' row and split them into two new rows in which one with value
+# for the first fuel type (value before the '/'), and one with value for the second fuel type
+# (value after the '/').
+
+# Create two copies of the hb_08 dataframe:
+df1 = hb_08.copy() # data on first fuel type of each hybrid vehicle
+df2 = hb_08.copy() # data on second fuel type of each hybrid vehicle
+
+# Now is the time to use python loop function along with apply and lambda function:
+# 1. Create a list of columns whose values need to be split by "/":
+split_columns = ['fuel', 'air_pollution_score', 'city_mpg', 'hwy_mpg', 'cmb_mpg', 'greenhouse_gas_score']
+
+# 2. apply split function to each column of each dataframe copy:
+for c in split_columns:
+    df1[c] = df1[c].apply(lambda x: x.split("/")[0])
+    df2[c] = df2[c].apply(lambda x: x.split("/")[1])
+
+
+# Now after successfully spliting and making 2 dataframes, we combine it together to make new_rows:
+new_rows = df1.append(df2)
+
+# now we can ignore the original hybrid rows where 2 values are sticked together:
+df_08.drop(hb_08.index, inplace=True)
+
+# add in our newly separated rows:
+df_08 = df_08.append(new_rows, ignore_index=True)
