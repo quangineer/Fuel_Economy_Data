3ea41563b06d99f115d63794627579879424de45
Quan Nguyen <quangineer@gmail.com>
2019-05-31 00:00:01 -0700

    updates

--- Conclusions&Visuals.py
+++ Conclusions&Visuals.py
@@ -5,20 +5,21 @@ df_08 = pd.read_csv("clean_08.csv")
 df_18 = pd.read_csv("clean_18.csv")
 
 # Are more unique models using alternative sources of fuel? By how much ?
-df_08_fuels_counts = df_08.fuel.value_counts()
-df_18_fuels_counts = df_18.fuel.value_counts()
-df_08_unique_models_alternative_fuel = df_08.query('fuel in ["CNG", "ethanol"]').model.nunique()
-df_18_unique_models_alternative_fuel = df_18.query('fuel in ["Electricity", "Ethanol"]').model.nunique()
-
-# plt.pyplot.bar(["2008", "2018"], [df_08_unique_models_alternative_fuel, df_18_unique_models_alternative_fuel]
-# plt.pyplot.title("Number of unique models using alternative fuels")
-# plt.pyplot.xlabel("Year")
-# plt.pyplot.ylabel("Number of unique models")
-# plt.pyplot.show()
-# df_08_unique_models_alternative_fuel.plot.bar(x="year", y='number')
+# df_08_fuels_counts = df_08.fuel.value_counts()
+# df_18_fuels_counts = df_18.fuel.value_counts()
+# df_08_unique_models_alternative_fuel = df_08.query('fuel in ["CNG", "ethanol"]').model.nunique()
+# df_18_unique_models_alternative_fuel = df_18.query('fuel in ["Electricity", "Ethanol"]').model.nunique()
+## PLOT: 
+# combine1 = pd.DataFrame({'year':['2008', '2018'], 'number':[df_08_unique_models_alternative_fuel, df_18_unique_models_alternative_fuel]})
+# ax = combine1.plot.bar(x='year', y='number', rot=0)
+# plt.pyplot.title('Number of models use alternative fuels in 2008 & 2018')
 # plt.pyplot.show()
 
-combine1 = pd.DataFrame({'year':['2008', '2018'], 'number':[df_08_unique_models_alternative_fuel, df_18_unique_models_alternative_fuel]})
-ax = combine1.plot.bar(x='year', y='number', rot=0)
-plt.pyplot.title('Number of models use alternative fuels in 2008 & 2018')
-plt.pyplot.show()
+# How much have vehilce classes improved in fuel economy?
+df_08.veh_class.value_counts()
+df_18.veh_class.value_counts()
+mean_of_08_VC_cmb_mpg = df_08.groupby('veh_class').cmb_mpg.mean()
+mean_of_18_VC_cmb_mpg = df_18.groupby('veh_class').cmb_mpg.mean()
+increase = mean_of_18_VC_cmb_mpg - mean_of_08_VC_cmb_mpg
+increase.dropna(inplace=True)
+print (increase)
